92a616f5fe190b933296e28e28e3cf82b029c0b1
Valbef <319310178+Valbef@users.noreply.github.com>
2026-08-24 06:19:20 +0000

    Add files via upload

--- SurvivalZtext/finales.py
+++ SurvivalZtext/finales.py
@@ -6,24 +6,24 @@ def comprobar_final(estado):
     if estado["vida"] <= 0:
 
         print("\nHas muerto.")
-        print("FINAL 1 - Fin del camino.")
+        write("FINAL 1 - Fin del camino.")
         sys.exit()
 
     if estado["hambre"] >= 100:
 
         print("\nMueres de hambre.")
-        print("FINAL 2 - Sin alimentos.")
+        write("FINAL 2 - Sin alimentos.")
         sys.exit()
 
     if estado["sed"] >= 100:
 
         print("\nNo encuentras agua.")
-        print("FINAL 3 - La sed pudo contigo.")
+        write("FINAL 3 - La sed pudo contigo.")
         sys.exit()
 
     if estado["moral"] <= 0:
 
-        writefast("\nPierdes completamente la esperanza y te dejas morir.")
+        writefast("\nPierdes completamente la esperanza.")
         write("FINAL 4 - Suicidio.")
         sys.exit()
 
